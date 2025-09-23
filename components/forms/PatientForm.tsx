a561d0a46c66802d0b990e257d22acadf083cc92
IrinaB <balkarovairina@gmail.com>
2025-09-23 09:03:32 +0200

    Add initial form components from shadcn

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,10 +1,50 @@
-import React from 'react'
+// noinspection TypeScriptValidateTypes
 
-function PatientForm(props) {
+'use client'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
+
+function PatientForm() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: '',
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
     return (
-        <div>
-
-        </div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+                <FormField
+                    control={form.control}
+                    name='username'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder='shadcn' {...field} />
+                            </FormControl>
+                            <FormDescription>This is your public display name.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type='submit'>Submit</Button>
+            </form>
+        </Form>
     )
 }
 
