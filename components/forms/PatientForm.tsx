b0491715a8962d0bae381560987c6c86dc2a5570
IrinaB <balkarovairina@gmail.com>
2025-09-24 14:10:10 +0200

    Add initial form for creating a user

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,45 +4,63 @@
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form } from '@/components/ui/form'
+import CustomFormField from '@/components/CustomFormField'
+import SubmitButton from '@/components/SubmitButton'
+import { useState } from 'react'
+import { UserFormValidation } from '@/lib/validation'
 
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-})
+export enum FormFieldType {
+    INPUT = 'input',
+    TEXTAREA = 'textarea',
+    PHONE_INPUT = 'phoneInput',
+    CHECKBOX = 'checkbox',
+    DATE_PICKER = 'datePicker',
+    SELECT = 'select',
+    SKELETON = 'skeleton'
+}
 
 function PatientForm() {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
         defaultValues: {
-            username: '',
+            name: '',
+            email: '',
+            phone: '',
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
+    async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+        setIsLoading(true)
+        try {
+            const userData = { name, email, phone }
+            console.log(userData);
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
-                <FormField
-                    control={form.control}
-                    name='username'
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder='shadcn' {...field} />
-                            </FormControl>
-                            <FormDescription>This is your public display name.</FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6 flex-1'>
+                <section className='mb-12 space-y-4'>
+                    <h1 className='header'>Hi there 👋</h1>
+                    <p className='text-dark-700'>Schedule your first appointment</p>
+                </section>
+                <CustomFormField fieldType={FormFieldType.INPUT} control={form.control}
+                                 name='name' label='Full name' placeholder='John Doe'
+                                 iconSrc='/assets/icons/user.svg' iconAlt='user' />
+                <CustomFormField fieldType={FormFieldType.INPUT} control={form.control}
+                                 name='email' label='Email' placeholder='johndoe@jsmastery.pro'
+                                 iconSrc='/assets/icons/email.svg' iconAlt='email' />
+                <CustomFormField fieldType={FormFieldType.PHONE_INPUT} control={form.control}
+                                 name='phone' label='Phone number' placeholder='(555) 123-4567'
                 />
-                <Button type='submit'>Submit</Button>
+                <SubmitButton isLoading={isLoading}>
+                    Get Started
+                </SubmitButton>
             </form>
         </Form>
     )
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,9 @@
+// noinspection TypeScriptValidateTypes
+
 'use client'
 
 import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    // @ts-ignore
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
